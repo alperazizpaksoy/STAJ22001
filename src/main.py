34693ffb8c47ed2274cca78a9d21655cad8c6182
Alper Aziz Paksoy <126792134+alperazizpaksoy@users.noreply.github.com>
2025-08-25 16:01:10 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
+from ttkbootstrap import Window
+from gui.main_window import URLExtractorGUI
+
+
+def main():
+    """Ana uygulama başlatma fonksiyonu"""
+    root = Window(themename="flatly")
+    app = URLExtractorGUI(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
 
